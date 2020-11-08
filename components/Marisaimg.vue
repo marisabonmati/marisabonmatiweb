379b0e7b8880c056aa2375a35cc2611b5651df28
marisabonmati <68008846+marisabonmati@users.noreply.github.com>
2020-11-08 19:00:51 +0100

    photo

---
+++ components/Marisaimg.vue
@@ -0,0 +1,18 @@
+<template>
+    <img class="Marisaimg" src="../static/marisa.jpeg"/>
+</template>
+
+<style>
+.Marisaimg {
+  animation: 3s appear;
+  margin: auto;
+  width: 35em;
+  padding-top: 5rem;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">marisabonmatiweb</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <Marisaimg />
+      <h1 class="title">Marisa Bonmatí</h1>
     </div>
   </div>
 </template>
